0d46aa25ac05e577414b3a8e50e2096c712ca46c
pkbtc <parasarkashyap06@gmail.com>
2024-04-30 13:11:41 +0530

    adding tailwind

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+
 import Web3Provider from './context/Web3Provider'
 import { RouterProvider } from 'react-router-dom';
 import { routes } from './routes/Routes';
--- frontend/client/src/components/UplaodImage.jsx
+++ frontend/client/src/components/UplaodImage.jsx
@@ -2,38 +2,47 @@ import axios from 'axios'
 import { useState } from 'react'
 import {useWeb3Context} from '../context/useWeb3Context';
 import toast from 'react-hot-toast';
+import {ImageUp} from 'lucide-react';
 const UplaodImage = () => {
   const {web3State}=useWeb3Context();
   const {contract,selectedAccount}=web3State;
+  const [loading,setLoading]=useState(false);
   console.log(contract,selectedAccount);
   const [file,setFile]=useState(null);
   const uplaodImageHash=async(ipfsHash)=>{
       const tx=await  contract.uploadFile(selectedAccount,ipfsHash);
-      await toast.promise(await tx.wait()),{
+      await toast.promise(tx.wait(),{
         loading:"transaction pending..",
         success:"image uploaded sucessfully",
         error:"transaction failed"
-      }
+      })
   }
   const hanldeUpload=async()=>{
     
     try {
+      setLoading(true);
       const formData=new FormData();
       formData.append("file",file);
       const url="http://localhost:3000/api/uploadimage";
-      const res=await axios.post(url,formData);
+    const res=await axios.post(url,formData);
+    
       toast.success("image uploaded sucessfully");
-      uplaodImageHash(res.data.ipfsHash);
+      await uplaodImageHash(res.data.ipfsHash);
+      setLoading(false);
+      
 
     } catch (error) {
       console.log(error)
+    }
+    finally{
+      setLoading(false);
     }
   }
   console.log(file);
   return (
     <div>
-      <input type='file' onChange={(e)=>setFile(e.target.files[0])}></input>
-      <button onClick={hanldeUpload}>Upload Image</button>
+      <input type='file' onChange={(e)=>setFile(e.target.files[0])} disabled={loading}></input>
+      <button onClick={hanldeUpload} disabled={loading || file}>Upload Image</button>
     </div>
   )
 }
