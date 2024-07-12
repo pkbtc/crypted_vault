1b5b5262e0bf3c3c7cf64874083891d54c9157ce
chkg2a <xshalltearvamps@gmail.com>
2024-07-12 10:43:01 +0530

    i forgor

--- frontend/client/src/components/NavBar.jsx
+++ frontend/client/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className=" w-full h-[80px] bg-sky-300 flex justify-center items-center border-b-2 border-gray-700">
          <ConnectedAccount />
-        </div> 
+    </div> 
   )
 }
 
